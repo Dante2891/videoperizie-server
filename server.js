a1bb6217277b8fbf96b597506b082e81c749da07
Dante2891 <lotti.bernardino@studiolotti.org>
2026-04-17 14:53:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: '*' }
-});
 
 app.use(express.json());
+
+const io = new Server(server, {
+  cors: { origin: '*', methods: ['GET', 'POST'] }
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 // Sessioni attive: { token: { operatore: socketId, cliente: socketId } }
 const sessioni = {};
